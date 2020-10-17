d1fcbf645c2983a3ac93cb0342dccf7a527bb77a
fandystar <jonasabok@gmail.com>
2020-10-17 20:54:30 +0700

    testing screen

--- App.js
+++ App.js
@@ -7,6 +7,70 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './Screens/SplashScreen';
+import { View, Text ,Button} from 'react-native'
+
+
+const  TestingScreen = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Testing !</Text>
+    
+    <Button
+      title='Testing'
+      onPress={() => props.navigation.navigate('TestingScreen')}
+    />
+    
+    <Button
+      title='Splash'
+      onPress={() => props.navigation.navigate('Splash')}
+    />
+   
+    <Button
+      title='Main'
+      onPress={() => props.navigation.navigate('Main')}
+    />
+    
+    
+    <Button
+      title='Login Screen'
+      onPress={() => props.navigation.navigate('Login')}
+    />
+    <Button
+      title='Register Screen'
+      onPress={() => props.navigation.navigate('Register')}
+    />
+    
+    <Button
+      title='Home Screen'
+      onPress={() => props.navigation.navigate('Home')}
+    />
+    <Button
+      title='Edit Profile'
+      onPress={() => props.navigation.navigate('EditProfile')}
+    />
+    {/* <Button
+      title='User Review'
+      onPress={() => props.navigation.navigate('User Review')}
+    />
+    <Button
+      title='Edit Profile'
+      onPress={() => props.navigation.navigate('Edit Profile')}
+    />
+     <Button
+      title='Play You Tube'
+      onPress={() => props.navigation.navigate('Play')}
+    />
+     <Button
+      title='list details component'
+      onPress={() => props.navigation.navigate('ListDetails')}
+    />  */}
+    
+  
+  </View>
+  )
+}
+
+
 
 
 const Stack = createStackNavigator();
@@ -14,11 +78,13 @@ const Stack = createStackNavigator();
 
 const RootStackScreen = ({ navigation }) => (
   <Stack.Navigator headerMode='none'>
-    {/* <Stack.Screen name="SplashScreen" component={SplashScreen} />
-    <Stack.Screen name="MainScreen" component={MainScreen} />
-    <Stack.Screen name="LoginScreen" component={LoginScreen} />
-    <Stack.Screen name="RegisterScreen" component={RegisterScreen} /> */}
-    <Stack.Screen name="EditprofileScreen" component={EditprofileScreen} />
+    <Stack.Screen name="Testing" component={TestingScreen} />
+    
+    <Stack.Screen name="Splash" component={SplashScreen} />
+    <Stack.Screen name="Main" component={MainScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
+    <Stack.Screen name="EditProfile" component={EditprofileScreen} />
 
   </Stack.Navigator>
 );
